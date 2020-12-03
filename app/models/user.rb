d4bd95847c4b20efcf290fcaeb1e34acec3fe0a0
EC2 Default User <ec2-user@ip-172-31-58-69.ec2.internal>
2020-12-03 10:50:03 +0000

    [add]ゲストログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   has_many :article_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+  end
 end
