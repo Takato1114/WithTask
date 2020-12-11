e00b926fc021c3a63e8f7d3b15a75eb84ca38b01
EC2 Default User <ec2-user@ip-172-31-58-69.ec2.internal>
2020-12-11 11:11:22 +0000

    [fix]マイグレーションファイルを修正

--- db/migrate/20201210114813_remove_task_id_from_articles.rb
+++ db/migrate/20201210114813_remove_task_id_from_articles.rb
@@ -1,7 +1,5 @@
 class RemoveTaskIdFromArticles < ActiveRecord::Migration[5.2]
   def change
-    remove_foreign_key :articles, :task_id
-    remove_reference :articles, :task_id
     remove_column :articles, :task_id, :integer
   end
 end
